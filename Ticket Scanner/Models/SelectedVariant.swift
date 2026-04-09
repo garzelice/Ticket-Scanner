b2a554e46e20a96ecc6ed6d626d2604f753e1203
Eric Wätke <email@ericwaetke.com>
2026-04-09 14:50:00 +0200

    huh

---
+++ Ticket Scanner/Models/SelectedVariant.swift
@@ -0,0 +1,21 @@
+//
+//  SelectedVariant.swift
+//  Ticket Scanner
+//
+//  Created by Eric Wätke on 17.03.25.
+//
+
+import Foundation
+
+struct SelectedVariant: Identifiable {
+	var id: String
+
+	var variant: Variants
+	var amount: Int
+
+	init(variant: Variants, amount: Int) {
+		id = variant.id ?? UUID().uuidString
+		self.variant = variant
+		self.amount = amount
+	}
+}
---
+++ Ticket Scanner/Models/ViewConfig.swift
@@ -0,0 +1,34 @@
+//
+//  ViewConfig.swift
+//  Ticket Scanner
+//
+//  Created by Eric Wätke on 17.03.25.
+//
+
+import Foundation
+
+struct ViewConfig: Identifiable, Equatable {
+	static func == (lhs: ViewConfig, rhs: ViewConfig) -> Bool {
+		return lhs.id == rhs.id
+	}
+
+	var id: String
+	var product: Product
+	var selectedVariants: [SelectedVariant]
+
+	init(product: Product, selectedVariants: [SelectedVariant]) {
+		id = product.id
+		self.product = product
+		self.selectedVariants = selectedVariants
+	}
+
+	static func example() -> ViewConfig {
+		return ViewConfig(product: Product.example(), selectedVariants: [])
+	}
+
+	static func examples() -> [ViewConfig] {
+		return Product.examples().map { product in
+			ViewConfig(product: product, selectedVariants: [])
+		}
+	}
+}
